049a5cb8e56b454819ef71a09052f959cbabc957
Rahul <talwarrahul295@gmail.com>
2025-10-14 17:34:17 +0530

    google deployment fix

--- src/components/pages/Auth/Login.jsx
+++ src/components/pages/Auth/Login.jsx
@@ -59,7 +59,7 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
-      const response = await fetch(`http://localhost:5000/api/auth/google`, {
+      const response = await fetch(`https://astrolozee-backend.vercel.app/api/auth/google`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include', // Important for cookies
