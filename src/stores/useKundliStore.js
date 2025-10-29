4a5b66f9c4c25dfe46ca1fdc839dae93edfa586e
Danish shafiq <67628974+dan1sh70@users.noreply.github.com>
2025-10-29 20:06:02 +0530

    Update API endpoint for generating Kundli

--- src/stores/useKundliStore.js
+++ src/stores/useKundliStore.js
@@ -9,7 +9,7 @@ const useKundliStore = create((set) => ({
     set({ loading: true });
 
     try {
-      const res = await axios.post("https://astrolozee-backend.vercel.app/api/kundli/generate",data,
+      const res = await axios.post("https://astrolozee-be.onrender.com/api/kundli/generate",data,
         { withCredentials: true }
       );
 
